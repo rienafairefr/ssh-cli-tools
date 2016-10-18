c87f5ba0bbc654a96307a10d7ead1b29670749ec
Alexandre Abadie <alexandre.abadie@inria.fr>
2016-10-18 16:26:53 +0200

    adding parser tests

---
+++ iotlabsshcli/tests/open_a8_parser_test.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+# This file is a part of IoT-LAB ssh-cli-tools
+# Copyright (C) 2015 INRIA (Contact: admin@iot-lab.info)
+# Contributor(s) : see AUTHORS file
+#
+# This software is governed by the CeCILL license under French law
+# and abiding by the rules of distribution of free software.  You can  use,
+# modify and/ or redistribute the software under the terms of the CeCILL
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# http://www.cecill.info.
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+
+"""Tests for iotlabsshcli.parser.open_a8 package."""
+
+import iotlabsshcli.parser.open_a8 as open_a8_parser
+
+from .iotlabsshcli_mock import MainMock
+from .compat import patch
+
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-few-public-methods
+
+
+class TestMainNodeParser(MainMock):
+    """Test open-a8-cli main parser."""
+
+    _nodes = ['a8-{0}.saclay.iot-lab.info'.format(i) for i in range(1, 6)]
+    _root_nodes = ['root@node-{0}'.format(node) for node in _nodes]
+
+    @patch('iotlabsshcli.open_a8.update_m3')
+    @patch('iotlabcli.parser.common.list_nodes')
+    def test_main_update_m3(self, list_nodes, update_m3):
+        """Run the parser.node.main with update-m3 subparser function."""
+
+        update_m3.return_value = {'result': 'test'}
+        list_nodes.return_value = self._nodes
+
+        args = ['update-m3', 'firmware.elf', '-l', 'saclay,a8,1-5']
+        open_a8_parser.main(args)
+        list_nodes.assert_called_with(self.api, 123, [self._nodes], None)
+        update_m3.assert_called_with({'user': 'username'}, self._root_nodes,
+                                     'firmware.elf')
+
+    @patch('iotlabsshcli.open_a8.reset_m3')
+    @patch('iotlabcli.parser.common.list_nodes')
+    def test_main_reset_m3(self, list_nodes, reset_m3):
+        """Run the parser.node.main with reset-m3 subparser function."""
+        reset_m3.return_value = {'result': 'test'}
+        list_nodes.return_value = self._nodes
+
+        args = ['reset-m3', '-l', 'saclay,a8,1-5']
+        open_a8_parser.main(args)
+        list_nodes.assert_called_with(self.api, 123, [self._nodes], None)
+        reset_m3.assert_called_with({'user': 'username'}, self._root_nodes)
+
+    def test_main_unknown_function(self):
+        """Run the parser.node.main with an unknown function."""
+        args = ['unknown-cmd']
+        self.assertRaises(SystemExit, open_a8_parser.main, args)
+
+    def test_open_a8_parse_and_run_unknown_function(self):
+        """Run the parser.node.main with an unknown function."""
+        args = ['unknown-cmd']
+        parser = open_a8_parser.parse_options()
+        self.assertRaises(TypeError, open_a8_parser.open_a8_parse_and_run,
+                          parser, args)
