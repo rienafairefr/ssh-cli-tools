f13783729c784d7c2f2228efa666b6b650eed638
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-04-06 15:28:58 +0200

    bump version

--- iotlabsshcli/__init__.py
+++ iotlabsshcli/__init__.py
@@ -20,4 +20,4 @@
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL license and that you accept its terms.
 
-__version__ = '0.1.0-dev'
+__version__ = '0.2.0-dev'
